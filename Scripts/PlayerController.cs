be219385353af629153cf7189035ff253b0dad08
John Reffitt <znothegamer@gmail.com>
2012-10-16 23:13:21 -0400

    Updated input to be in all axis and more fluid
    The input was switched to the string-based axis for the input manager.

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,26 +2,46 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-	public float speed = 40.0f;
-
-	// Use this for initialization
-	void Start () {
 	
-	}
+	public float MAX_SPEED = 90.0f;
+	public float attackSpeed = 0.5f;
+	private float _atkSpd = 0.0f;
+	private Vector3 uForce = Vector3.zero;
 	
+	public GameObject bulletPrefab;
+		
 	// Update is called once per frame
 	void Update () {
 	
-	
-		if(Input.GetKey(KeyCode.LeftArrow))
+		uForce = Vector3.zero;
+		
+		uForce.x += Input.GetAxis("Horizontal");
+		uForce.y += Input.GetAxis("Vertical");
+		uForce.Normalize();
+		
+		if( uForce != Vector3.zero )
 		{
-			transform.localPosition -= (-Vector3.right) * speed * Time.deltaTime;
+			Debug.Log(uForce);
+			rigidbody.AddForce(MAX_SPEED * uForce);
 		}
-		if( Input.GetKey(KeyCode.RightArrow) )
+		else
 		{
-			transform.localPosition += (-Vector3.right) * speed * Time.deltaTime;
+			rigidbody.velocity = rigidbody.velocity * 0.99f;
 		}
+		
+		if( Input.GetKey(KeyCode.Space) && _atkSpd <= 0.0f)
+		{
+			FireWeapon();
+			_atkSpd = attackSpeed;
+		}
+		if( _atkSpd > 0)
+			_atkSpd -= Time.deltaTime;
 	
 	}
+	
+	public void FireWeapon()
+	{
+		// TODO: Enable the weapon to fire.
+	}
+	
 }
